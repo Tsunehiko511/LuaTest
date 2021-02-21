5f4632ce6a738905b6f915818cae21678d5d4801
嶋津恒彦 <simanezumi1989@gmail.com>
2021-02-22 02:59:16 +0900

    Luaによるゲームイベントの実装

---
+++ Assets/Scripts/GameEventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameEventManager : MonoBehaviour
+{
+    [SerializeField] EventData eventData = default;
+    [SerializeField] Player player = default;
+    [SerializeField] TextAsset textAsset = default;
+
+    private void Start()
+    {
+        StartCoroutine(ExecuteEvent(textAsset.text));
+    }
+
+    IEnumerator ExecuteEvent(string script)
+    {
+        var interpreter = new LuaInterpreter(script); // スクリプトを渡して初期化
+        interpreter.AddHandler("player", player); // メッセージ制御のハンドラを登録
+        interpreter.AddHandler("event", eventData); // メッセージ制御のハンドラを登録
+        while (interpreter.HasNextScript())
+        {
+            interpreter.Resume();
+            yield return interpreter.WaitCoroutine();
+        }
+    }
+}
